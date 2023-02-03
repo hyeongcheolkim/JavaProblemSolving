63137a9b0b15c21c4cc28a3cf99ab726f800ac36
hyeongcheolkim <tori1753@naver.com>
2023-02-03 15:31:34 +0900

    q1000

--- src/baekjoon/q1000/Main.java
+++ src/baekjoon/q1000/Main.java
@@ -1,4 +1,23 @@
 package baekjoon.q1000;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
+
+    void solution() throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        var sb = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(sb.nextToken());
+        int b = Integer.parseInt(sb.nextToken());
+
+        bw.write(String.valueOf(a + b));
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
 }
